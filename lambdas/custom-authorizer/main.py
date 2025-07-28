86c74d6351eb04af851552550ead968bdcc0543a
mashwin <ashwinmorey@gmail.com>
2025-07-28 17:37:56 +0530

    updated code

--- lambdas/custom-authorizer/main.py
+++ lambdas/custom-authorizer/main.py
@@ -1,29 +1,29 @@
 import json
 
 def lambda_handler(event, context):
-    print("=== Custom Authorizer Invoked ===")
-    print("Authorization Token:", event.get('authorizationToken'))
-    print("Method ARN:", event.get('methodArn'))
+    headers = event.get('headers', {})
+    token = headers.get('Authorization')
 
-    token = event.get('authorizationToken')
+    print(f"Headers: {headers}")
+    print(f"Authorization Token: {token}")
+
     method_arn = event.get('methodArn')
 
     if token == "allow":
-        print("✔ Token matched 'allow'. Returning Allow policy.")
         return generate_policy("user", "Allow", method_arn)
     else:
-        print("❌ Token did NOT match. Returning Deny policy.")
         return generate_policy("user", "Deny", method_arn)
 
 def generate_policy(principal_id, effect, resource):
-    return {
-        "principalId": principal_id,
-        "policyDocument": {
-            "Version": "2012-10-17",
-            "Statement": [{
-                "Action": "execute-api:Invoke",
-                "Effect": effect,
-                "Resource": resource
-            }]
+    if effect and resource:
+        return {
+            "principalId": principal_id,
+            "policyDocument": {
+                "Version": "2012-10-17",
+                "Statement": [{
+                    "Action": "execute-api:Invoke",
+                    "Effect": effect,
+                    "Resource": resource
+                }]
+            }
         }
-    }

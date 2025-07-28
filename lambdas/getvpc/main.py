1d838b04b94a32c27f1a6ce6e2fa88ab786d495f
mashwin <ashwinmorey@gmail.com>
2025-07-28 10:27:50 +0530

    getvpc lambda

---
+++ lambdas/getvpc/main.py
@@ -0,0 +1,32 @@
+import boto3
+import os
+import json
+
+dynamodb = boto3.resource('dynamodb')
+TABLE_NAME = os.environ.get("DYNAMODB_TABLE_NAME", "vpc-provisioning-table")
+table = dynamodb.Table(TABLE_NAME)
+
+def lambda_handler(event, context):
+    try:
+        # Get vpc_id from path parameters
+        vpc_id = event['pathParameters']['vpc_id']
+        
+        # Fetch item from DynamoDB
+        response = table.get_item(Key={'vpc_id': vpc_id})
+        
+        if 'Item' not in response:
+            return {
+                'statusCode': 404,
+                'body': json.dumps({'message': f'VPC ID {vpc_id} not found'})
+            }
+        
+        return {
+            'statusCode': 200,
+            'body': json.dumps(response['Item'])
+        }
+    
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
+        }
